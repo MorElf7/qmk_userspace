ab932309af7a77c9cc81c848b7572acdb5a4dc42
Steve Cao <steve.cao4502@gmail.com>
2024-12-13 20:47:51 -0500

    update super and alt key for first layer

--- keyboards/keychron/v1/ansi_encoder/keymaps/morelf7/keymap.c
+++ keyboards/keychron/v1/ansi_encoder/keymaps/morelf7/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,    KC_PGUP,
         ESC_CTRL,  GUI_A,    ALT_S,    CTL_D,    SFT_F,    KC_G,     KC_H,     SFT_J,    CTL_K,    ALT_L,    GUI_SCLN, KC_QUOT,         KC_ENT,         KC_PGDN,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,    KC_UP,
-        KC_LCTL,  KC_LALT,  KC_LGUI,                      KC_SPC,                 KC_RGUI,  MO(LINUX_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,                 KC_RALT,  MO(LINUX_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [LINUX_FN] = LAYOUT_ansi_82(
         QK_BOOTLOADER,  KC_BRID,  KC_BRIU,  KC_MCTL,  KC_SLEP,  KC_WAKE,  KC_NO,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,   _______,            _______,
